66179e1c13da55a0f6afca9b72818fd649518de3
Raymond Lee <riemann.lei@gmail.com>
2018-06-04 15:13:40 -0500

    Initial commit;

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+               Auto-generated when using Eclipse CDT IDE.
+
+               Procedures:
+               1. Move mouse cursor to Project Explorer area then right-click
+               2. Select New -> C/C++ Project
+               3. Select "C Managed Build", click Next
+               4. Project type: Choose Executable -> Hellow World ANSI C Project
+               5. Toolchain: MinGW GCC
+               6. Name your project at the top textbox: HelloWorld
+               7. Click Finish
+
+               Compilation steps:
+               1. Click Project -> Build All, wait for compilation finish
+               2. Click Run -> Run to execute
+               3. Confirm "Hello World" is shown in Console window
+
+ ============================================================================
+ */
+
+#include <stdio.h>    //for puts, printf
+#include <stdlib.h>   //for EXIT_SUCCESS
+
+int main(void)
+{
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
